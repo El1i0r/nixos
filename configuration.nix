9be39da9437e003ca582b61538341b99c89a2d67
El1i0r <el1i0r.main@gmail.com>
2024-08-12 18:07:32 +0500

    idiot

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,49 @@
   services.greetd.package = pkgs.greetd.regreet;
   services.greetd.enable = true;
   programs.regreet.enable = true;
+  programs.regreet.settings = {
+  
+[background]
+# Path to the background image
+path = "/home/el1i0r/Downloads/r.jpg"
+
+# How the background image covers the screen if the aspect ratio doesn't match
+# Available values: "Fill", "Contain", "Cover", "ScaleDown"
+# Refer to: https://docs.gtk.org/gtk4/enum.ContentFit.html
+# NOTE: This is ignored if ReGreet isn't compiled with GTK v4.8 support.
+fit = "Contain"
+
+# The entries defined in this section will be passed to the session as environment variables when it is started
+[env]
+ENV_VARIABLE = "value"
+
+[GTK]
+# Whether to use the dark theme
+application_prefer_dark_theme = true
+
+# Cursor theme name
+cursor_theme_name = "Adwaita"
+
+# Font name and size
+font_name = "Cantarell 16"
+
+# Icon theme name
+icon_theme_name = "Reversal-Orange"
+
+# GTK theme name
+theme_name = "Colloid-Orange-Dark-Gruvbox"
+
+[commands]
+# The command used to reboot the system
+reboot = [ "systemctl", "reboot" ]
+
+# The command used to shut down the system
+poweroff = [ "systemctl", "poweroff" ]
+
+[appearance]
+# The message that initially displays on startup
+greeting_msg = "Welcome back!" 
+  };
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
