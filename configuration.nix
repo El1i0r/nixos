2c86cf321b2c93af501b649f01020d705bb0d166
El1i0r <el1i0r.main@gmail.com>
2024-08-12 17:08:35 +0500

    idiot

--- configuration.nix
+++ configuration.nix
@@ -67,7 +67,7 @@
   users.defaultUserShell = pkgs.xonsh;
   programs.xonsh.enable = true;
   # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Enable CUPS to print documents.
