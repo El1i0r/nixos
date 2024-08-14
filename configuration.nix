b9e4e54e39adf0e4e3621e9c56e14592b87054c0
El1i0r <el1i0r.main@gmail.com>
2024-08-14 09:59:20 +0500

    changes

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./overlays.nix
-      ./xonsh.nix
+      #./xonsh.nix
       inputs.home-manager.nixosModules.default
     ];
   home-manager = {
@@ -75,57 +75,7 @@
   programs.zsh.enable = true;
   users.defaultUserShell = config.programs.xonsh.package;
   programs.xonsh.enable = true;
-  programs.xonsh.package = pkgs.xonsh.wrapper.override { extraPackages = ps: [
-  (ps.buildPythonPackage rec {
-    name = "xontrib-sh";
-    version = "0.3.1";
-
-    src = pkgs.fetchFromGitHub {
-    owner = "anki-code";
-      repo = "${name}";
-      rev = "${version}";
-      sha256 = "KL/AxcsvjxqxvjDlf1axitgME3T+iyuW6OFb1foRzN8=";
-    };
-
-    meta = {
-      homepage = "https://github.com/anki-code/xontrib-sh";
-      description = "Paste and run commands from bash, fish, zsh, tcsh in xonsh shell.";
-      license = pkgs.lib.licenses.mit;
-      maintainers = [ ];
-    };
-
-    prePatch = ''
-   #     pkgs.lib.substituteInPlace pyproject.toml --replace '"xonsh>=0.12.5"' ""
-    '';
-   #  patchPhase = "sed -i -e 's/^dependencies.*$/dependencies = []/' pyproject.toml";
-   # doCheck = false;
-  })
-  (ps.buildPythonPackage rec {
-    name = "xontrib-fish-completer";
-    version = "0.0.1";
-
-    src = pkgs.fetchFromGitHub {
-    owner = "xonsh";
-      repo = "${name}";
-      rev = "${version}";
-      sha256 = "sha256-PhhdZ3iLPDEIG9uDeR5ctJ9zz2+YORHBhbsiLrJckyA=";
-    };
-
-    meta = {
-      homepage = "https://github.com/xonsh/xontrib-fish-completer";
-      description = "Populate rich completions using fish and remove the default bash based completer";
-      license = pkgs.lib.licenses.mit;
-      maintainers = [ ];
-    };
-
-    prePatch = ''
-      pkgs.lib.substituteInPlace pyproject.toml --replace '"xonsh>=0.12.5"' ""
-    '';
-    patchPhase = "sed -i -e 's/^dependencies.*$/dependencies = []/' pyproject.toml";
-    doCheck = false;
-  })
-  ]; };
-
+  # programs.xonsh.package = pkgs.xonsh;
   # Enable the GNOME Desktop Environment.
   services.xserver.desktopManager.gnome.enable = true;
   #DM
