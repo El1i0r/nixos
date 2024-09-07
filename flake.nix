25824183ad810071a9979681063e68023544a3a0
El1i0r <el1i0r.main@gmail.com>
2024-09-07 11:11:47 +0500

    COSMIC flake

--- flake.nix
+++ flake.nix
@@ -7,11 +7,12 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   nixos-cosmic = {
-    url = "github:lilyinstarlight/nixos-cosmic";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { nixpkgs, home-manager, self, nixos-cosmic, ... }@inputs:
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  }
+  outputs =
+    { nixpkgs, home-manager, self, nixos-cosmic  ... }@inputs:
     let
       system = "x86_64-linux";
       host = "ORBIUM-A5";
@@ -27,6 +28,7 @@
             inherit host;
           };
           modules = [
+
             ./hosts/${host}/config.nix
             home-manager.nixosModules.home-manager
             {
@@ -39,17 +41,16 @@
               home-manager.useUserPackages = true;
               home-manager.backupFileExtension = "backup";
               home-manager.users.${username} = import ./hosts/${host}/home.nix;
-            }
+            };
             {
-              nix.settings = {
-                substituters = [ "https://cosmic.cachix.org/" ];
-                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-              };
-            }
-            nixos-cosmic.nixosModules.default
+               nix.settings = {
+               substituters = [ "https://cosmic.cachix.org/" ];
+               trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+               };
+            };
+              nixos-cosmic.nixosModules.default
           ];
         };
       };
     };
 }
-
