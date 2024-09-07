0bc17467fc2a3b4b974f0220a33b26eb39c6cc0c
El1i0r <el1i0r.main@gmail.com>
2024-09-07 11:18:03 +0500

    COSMIC flake

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
                substituters = [ "https://cosmic.cachix.org/" ];
                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
                };
-            };
+            }`
               nixos-cosmic.nixosModules.default
           ];
         };
