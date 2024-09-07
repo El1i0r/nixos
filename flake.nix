b794016fb2e934334d8eb208fca7151eb261cfcf
El1i0r <el1i0r.main@gmail.com>
2024-09-07 11:13:50 +0500

    COSMIC flake

--- flake.nix
+++ flake.nix
@@ -5,12 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-  nixos-cosmic = {
+    nixos-cosmic = {
       url = "github:lilyinstarlight/nixos-cosmic";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  }
+  };
   outputs =
     { nixpkgs, home-manager, self, nixos-cosmic  ... }@inputs:
     let
