48164bff761a49cc7600903fe2e518b7269b7747
El1i0r <el1i0r.main@gmail.com>
2024-09-07 11:03:36 +0500

    COSMIC flake

--- flake.nix
+++ flake.nix
@@ -6,8 +6,15 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
+  nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  
+ 
   outputs =
-    { nixpkgs, home-manager, ... }@inputs:
+    { nixpkgs, home-manager, self, nixos-cosmic  ... }@inputs:
     let
       system = "x86_64-linux";
       host = "ORBIUM-A5";
@@ -23,6 +30,7 @@
             inherit host;
           };
           modules = [
+
             ./hosts/${host}/config.nix
             home-manager.nixosModules.home-manager
             {
@@ -36,6 +44,13 @@
               home-manager.backupFileExtension = "backup";
               home-manager.users.${username} = import ./hosts/${host}/home.nix;
             }
+            {
+            nix.settings = {
+              substituters = [ "https://cosmic.cachix.org/" ];
+              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+            };
+            nixos-cosmic.nixosModules.default
+          }
           ];
         };
       };
